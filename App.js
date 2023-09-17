5327e57d9ac868c904938dd07ec3c7626ea27027
kgilbert78 <kgilbert78@gmail.com>
2023-09-16 22:18:55 -0400

    started styling, fonts not working yet

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { useFonts, RubikMonoOne_400Regular } from '@expo-google-fonts/rubik-mono-one';
 import { useEffect, useState } from "react";
 import {
   calculateSwitchTime,
@@ -10,6 +10,14 @@ import {
 // import About from "./About";
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    RubikMonoOne_400Regular,
+  });
+
+  // if (!fontsLoaded && !fontError) {
+  //   return null;
+  // }
+
   const [isEven, setIsEven] = useState(determineParkingSide());
   const [switchTime, setSwitchTime] = useState(calculateSwitchTime());
   const [foolsDay, setFoolsDay] = useState(isFoolsDay());
@@ -27,29 +35,29 @@ export default function App() {
   }, []);
 
   return (
-    <View
+    <View style={isEven ? styles.evenContainer : styles.oddContainer}
       className="App"
     >
-      <Text>side</Text>
-      {isEven ? <Text>Even</Text> : <Text>Odd</Text>}{/* h1 */}
-      <Text>{new Date().getHours() < 18 ? "Before" : "After"} 6 pm</Text>{/* h2 */}
-      <Text>
+      <Text style={styles.plainText}>side</Text>
+      {isEven ? <Text style={styles.heading}>Even</Text> : <Text style={styles.heading}>Odd</Text>}
+      <Text style={styles.heading2}>{new Date().getHours() < 18 ? "Before" : "After"} 6 pm</Text>
+      <Text style={styles.heading2}>
         On{" "}
         {new Date().toLocaleDateString("default", {
           month: "long",
           day: "numeric",
         })}
-      </Text>{/* h2 */}
+      </Text>
       {foolsDay && (
         <>
-          <Text>Warning: Fools day!</Text>{/* h2 */}
+          <Text style={styles.heading2}>Warning: Fools day!</Text>
           <Text>Do not move your car tonight! Tomorrow is also an odd day.</Text>
         </>
       )}
-      <Text>Next switch in:</Text>
-      <Text>
+      <Text style={styles.plainText}>Next switch in:</Text>
+      <Text style={styles.heading3}>
         {hoursToSwitch} hours and {minutesToSwitch % 60} minutes
-      </Text>{/* h3 */}
+      </Text>
 
       {/* TO DO: Translate About contents from HTML to react-native built-in components like <View> and <Text> */}
       {/* <About isEven={isEven} foolsDay={foolsDay} /> */}
@@ -59,10 +67,39 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  evenContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#3369ff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    padding: 20,
+
   },
+  oddContainer: {
+    flex: 1,
+    backgroundColor: '#f75231',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    padding: 20,
+  },
+  heading: {
+    fontFamily: 'RubikMonoOne-Regular',
+    fontSize: 80,
+    color: 'white',
+    fontWeight: '800',
+    textAlign: 'left'
+  },
+  heading2: {
+    fontSize: 50,
+    color: 'black',
+    fontWeight: '800'
+  },
+  heading3: {
+    fontSize: 30,
+    color: 'black',
+    fontWeight: '800'
+  },
+  plainText: {
+    fontSize: 20,
+  }
 });
